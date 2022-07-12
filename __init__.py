3fc3d77d4f30bff664bab8bb2bd19283ec5b8648
Daniel McKnight <daniel@neon.ai>
2022-07-12 12:46:36 -0700

    Wait for response in WW change methods to ensure config updates before speaking confirmation

--- __init__.py
+++ __init__.py
@@ -99,8 +99,8 @@ class DeviceControlCenterSkill(NeonSkill):
                 resp = self.ask_yesno("ask_start_skipping")
                 if resp == "yes":
                     self.speak_dialog("confirm_skip_ww", private=True)
-                    self.bus.emit(message.forward("neon.wake_words_state",
-                                                  {"enabled": False}))
+                    self.bus.wait_for_response(message.forward(
+                        "neon.wake_words_state", {"enabled": False}))
                 else:
                     self.speak_dialog("not_doing_anything", private=True)
             else:
@@ -119,8 +119,8 @@ class DeviceControlCenterSkill(NeonSkill):
             resp = self.ask_yesno("ask_start_requiring")
             if resp == "yes":
                 self.speak_dialog("confirm_require_ww", private=True)
-                self.bus.emit(message.forward("neon.wake_words_state",
-                                              {"enabled": True}))
+                self.bus.wait_for_response(message.forward(
+                    "neon.wake_words_state", {"enabled": True}))
             else:
                 self.speak_dialog("not_doing_anything", private=True)
         else:
